2ac1ab9939b30589a53aa2e45c3477c869f66c34
mtaku3 <m.taku3.1222@gmail.com>
2024-05-02 14:47:15 +0900

    fix!: remove logic :) i am playing on hard mode xD

--- src/service/domain/User/IRepository.ts
+++
@@ -1,7 +0,0 @@
-import { TraqId, User } from "./Entity";
-
-export interface IUserRepository {
-  findByTraqId(traqId: TraqId): Promise<User | null>;
-  create(user: User): Promise<void>;
-  update(user: User): Promise<void>;
-}
