49abc4a2f3baf982be6318af5861228e86278e2d
mtaku3 <m.taku3.1222@gmail.com>
2024-05-18 16:29:21 +0900

    fix: fix prod entrypoint

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -4,9 +4,8 @@ set -e
 infisical run \
   --projectId 596480d8-07e7-4a4a-8309-1ed846a77923 \
   --env prod \
-  --command "
-  npm run build \
+  --command \
+  "npm run build \
   && npx prisma migrate deploy \
   && npx prisma seed \
-  && $@
-  "
+  && $@"
