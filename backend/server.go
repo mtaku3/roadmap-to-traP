af866e1d168c7c119baeaa6dc3a055963944bf34
mtaku3 <m.taku3.1222@gmail.com>
2024-10-10 14:26:14 +0900

    feat: add gorm

--- backend/server.go
+++ backend/server.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
 )
 
 func main() {
+	dsn := "root:password@tcp(127.0.0.1:3306)/dev?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
