3578cb30099ad1e053105f0bfb2abaa961e92c91
mtaku3 <m.taku3.1222@gmail.com>
2024-10-11 13:52:17 +0900

    feat: add tailwindcss

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./index.css";
 
 function Index() {
   const [count, setCount] = useState(0);
