23340cd69915fe976807834f763f87e386e52161
mtaku3 <m.taku3.1222@gmail.com>
2024-10-11 13:40:32 +0900

    feat: init vite.js react

--- .nvim.lua
+++ .nvim.lua
@@ -2,6 +2,8 @@ local lsp = require("lsp")
 local none_ls = require("none-ls")
 
 lsp.setup("gopls", {})
+lsp.setup("tsserver", {})
 none_ls.register({
-  none_ls.builtins.formatting.gofmt
+  none_ls.builtins.formatting.gofmt,
+  none_ls.builtins.formatting.prettierd
 })
